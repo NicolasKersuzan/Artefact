22f2ff25327bba682ea60613c68613dea5f8652d
NicolasKersuzan <n.kersuzan@ag2l.fr>
2016-09-12 14:17:21 +0200

    modification du powershell

--- Artefacts/windows-sqlserverrenameinstance/RenameSQLServerInstance.ps1
+++ Artefacts/windows-sqlserverrenameinstance/RenameSQLServerInstance.ps1
@@ -1,40 +1,104 @@
-﻿
-
-stop-service "SQLSERVERAGENT"
-[INT]$ServiceNb = 0
-[String]$Service = get-service -Name "MSSQLSERVER" | foreach {$_.Status}
-
-If ($Service -eq "Stopped")
+﻿Function WaitForStartService($name)
 {
-    start-service "MSSQLSERVER"  
+    [ARRAY]$ServiceMSSQL
+    $ServiceNb = 0
+    While ($ServiceNb -eq 0)
+    {
+        $ServiceMSSQL = get-service -Name $name | Where-Object {$_.Status -eq "Running"}  | foreach {$_.Status}
+        $ServiceNb = $ServiceMSSQL.count
+        Write-host "Attente du démarrage du service $name"
+        get-service -Name $name | Where-Object {$_.Status -eq "Running"}  | foreach ($_.Status)
+    }
 }
-[ARRAY]$ServiceMSSQL
-$ServiceNb = 0
 
-
-SQLCMD -S "HYDRA" -Q "EXEC SP_DROPSERVER 'HYDRATEST'"
-SQLCMD -S "HYDRA" -Q "EXEC SP_ADDSERVER 'HYDRA','local'"
-stop-service "MSSQLSERVER"
-
-$ServiceNb = 0
-While ($ServiceNb -eq 0)
+Function IsStartSQLServer($name)
 {
-    $ServiceMSSQL = get-service -Name "MSSQLSERVER" | Where-Object {$_.Status -eq "Stopped"}  | foreach {$_.Status}
+    [ARRAY]$ServiceMSSQL
+    $ServiceMSSQL = get-service -Name $name | Where-Object {$_.Status -eq "Running"}  | foreach {$_.Status}
+    $ServiceNb =$ServiceMSSQL.count
+    If  ($ServiceNb -gt 0)
+    {
+        return $true
+    }
+    Else
+    {
+        return $false
+    }
+    
+}
+Function IsStopSQLServer($name)
+{
+    [ARRAY]$ServiceMSSQL    
+    $ServiceMSSQL = get-service -Name $name | Where-Object {$_.Status -eq "Stopped"}  | foreach {$_.Status}
     $ServiceNb = $ServiceMSSQL.count
-    Write-host $ServiceMSSQL.count
-    get-service -Name "MSSQLSERVER" | Where-Object {$_.Status -eq "Stopped"}  | foreach ($_.Status)
+    If  ($ServiceNb -gt 0)
+    {
+        return $true
+    }
+    Else
+    {
+        return $false
+    }
 }
 
-start-service "MSSQLSERVER"
-$ServiceNb = 0
-While ($ServiceNb -eq 0)
+Function WaitForStopService($name)
 {
-    $ServiceMSSQL = get-service -Name "MSSQLSERVER" | Where-Object {$_.Status -eq "Running"}  | foreach {$_.Status}
-    $ServiceNb = $ServiceMSSQL.count
-    Write-host $ServiceMSSQL.count
-    get-service -Name "MSSQLSERVER" | Where-Object {$_.Status -eq "Running"}  | foreach ($_.Status)
+    [ARRAY]$ServiceMSSQL
+    $ServiceNb = 0
+    While ($ServiceNb -eq 0)
+    {
+        $ServiceMSSQL = get-service -Name $name | Where-Object {$_.Status -eq "Stopped"}  | foreach {$_.Status}
+        $ServiceNb = $ServiceMSSQL.count
+        Write-host "Attente de l'arrêt du service $name"
+        get-service -Name $name | Where-Object {$_.Status -eq "Stopped"}  | foreach ($_.Status)
+    }
 }
-Start-service "SQLSERVERAGENT"
+
+#MSSQLSERVER
+#SQLSERVERAGENT
+
+Try { 
+
+    if (IsStartSQLServer("SQLSERVERAGENT") -eq $true)
+    {
+        stop-service "SQLSERVERAGENT"
+    }
+    WaitForStopService("SQLSERVERAGENT")
+    
+    if (IsStartSQLServer("SQLSERVERAGENT") -eq $false)
+    {
+       start-service "MSSQLSERVER" 
+    }
+
+    $ComputerName = $env:COMPUTERNAME
+    $Result = $(sqlcmd -S $ComputerName -Q "SELECT Name FROM sys.servers WHERE Server_Id = 0")
+    $OldComputerName = $Result[2]
+
+    # suppression de l'ancien nom de l'instance
+    $cmd = 'EXEC SP_DROPSERVER ' + "`'$($OldComputerName) + `'"
+    SQLCMD -S  $($ComputerName) -Q `"$($cmd)`"
+
+    # ajout du nouveau nom de l'instance
+    $cmd = "EXEC SP_ADDSERVER  `'$($ComputerName)`', `'local`'" 
+    SQLCMD -S $($ComputerName) -Q $($cmd)
+    stop-service "MSSQLSERVER"
+
+    if (IsStopSQLServer("MSSQLSERVER") -eq $true)
+    {
+        start-service "MSSQLSERVER"
+    }
+
+    WaitForStartService("MSSQLSERVER")
+    Start-service "SQLSERVERAGENT"
+    WaitForStartService("SQLSERVERAGENT")
+}
+catch
+{
+    throw ($Error[0])
+}
+
+
+
 
 
 
